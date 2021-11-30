97b6ba82f7fb9ba1fc47c321f8532f501633037d
N Kort <studio@n-kort.net>
2021-11-30 18:22:39 +0100

    internal links in map block description

--- src/components/blocks/MemoryMapBlock.vue
+++ src/components/blocks/MemoryMapBlock.vue
@@ -6,7 +6,7 @@
       <div class="max-w-6xl mx-auto my-8 md:my-12">
         <p class="text-2xl mb-2">{{ title }}</p>
         <div class="max-w-prose-1 html">
-          <div v-html="description"></div>
+          <div v-html="description" @click="internalLinks"></div>
         </div>
       </div>
 
@@ -18,6 +18,7 @@
 <script lang="ts">
   import { defineComponent, onMounted } from 'vue'
   import { useStore } from '../../store'
+  import { useRoute, useRouter } from 'vue-router'
   import StyledMap from '../StyledMap.vue'
   import MarkersList from '../MarkersList.vue'
 
@@ -35,7 +36,19 @@
         store.getMarkers()
       })
 
-      return { title, description }
+      const route = useRoute()
+      const router = useRouter()
+      const internalLinks = (event: Event ) => {
+        const el = event.target as HTMLElement
+        if (el && (el.tagName === 'A' || el.tagName === 'a')) {
+          event.preventDefault() // check first
+          const path = el.getAttribute('href')
+          console.log('go to', path)
+          router.push(`#kontext=${path}`)
+        }
+      }
+
+      return { title, description, internalLinks }
     },
     components: { StyledMap, MarkersList }
   })
